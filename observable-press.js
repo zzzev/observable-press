b5b5ba9debcda7b829cd14befc3f05a1ce6305b7
Zev Youra <zevyoura@gmail.com>
2018-10-06 10:30:18 -0700

    lots of polish, separated js

--- observable-press.js
+++ observable-press.js
@@ -1,44 +1,18 @@
-import {Library, Runtime} from 'https://unpkg.com/@observablehq/notebook-runtime?module';
+import {Runtime} from 'https://unpkg.com/@observablehq/notebook-runtime?module';
 
-// If you cannot use dynamic imports, use a static import like this:
-// import notebook from 'https://api.observablehq.com/d/895ffec7eb2c9e1b.js'
-
-const getNotebookJsUrl = (id) => `https://api.observablehq.com/${id}.js`;
-const getNotebookUrl = (id) => `https://beta.observablehq.com/${id}`;
-
-// A convenience function that returns a promise and its
-// resolve and reject callbacks.
-function getPromiseParts() {
-  let parts = {};
-  parts.promise = new Promise((resolve, reject) => {
-    parts.resolve = resolve;
-    parts.reject = reject;
-  });
-  return parts;
-}
-
-function showError(errorHtml) {
-  document.querySelector('.loading').innerHTML = `<span>${errorHtml}</span>`;
-}
-
-async function bootstrap() {
-  const notebookId = document.querySelector('meta[name="notebook"]').content;
-
-  let notebook;
-  try {
-    notebook = (await import(getNotebookJsUrl(notebookId))).default;
-  } catch (e) {
-    showError(`Could not import notebook with id ${notebookId}.
-    Are you sure it exists and is available?`);
-  }
+/* Usage:
+ * ```
+ * import notebook from 'https://api.observablehq.com/@zzzev/my-notebook.js';
+ * import bootstrap from 'observable-press';
+ * bootstrap(notebook);
+ * ```
+ */
+export default async function bootstrap(notebook) {
+  const notebookId = notebook.id;
 
   let firstRenderPromises = [];
 
   Runtime.load(notebook, (cell) => {
-    if (!cell.name) {
-      // Ignore un-named cells
-      return;
-    }
     if (cell.name === 'title') {
       let promiseParts = getPromiseParts();
       firstRenderPromises.push(promiseParts.promise);
@@ -81,14 +55,35 @@ async function bootstrap() {
   });
   
   try {
+    console.log(`awaiting ${firstRenderPromises.length} cells`);
     await Promise.all(firstRenderPromises);
     // remove first load indicators once all watched nodes have rendered first time
     document.querySelectorAll('.loading').forEach(node => node.remove());
   } catch (e) {
+    console.error(e);
     showError(`An error occured while loading this notebook. Visit the
       <a href="${getNotebookUrl(notebookId)}">original notebook</a>
       for more information.`);
   }
 }
 
-bootstrap();
+
+// Helper functions:
+const getNotebookUrl = (id) => {
+  return `https://beta.observablehq.com/${id[0] === '@' ? id : 'd/' + id.replace(/'@.*'/, '')}`
+};
+
+// A convenience function that returns a promise and its
+// resolve and reject callbacks.
+function getPromiseParts() {
+  let parts = {};
+  parts.promise = new Promise((resolve, reject) => {
+    parts.resolve = resolve;
+    parts.reject = reject;
+  });
+  return parts;
+}
+
+function showError(errorHtml) {
+  document.querySelector('.loading').innerHTML = `<span>${errorHtml}</span>`;
+}
---
+++ trumpbits.js
@@ -0,0 +1,4 @@
+import notebook from 'https://api.observablehq.com/@zzzev/trump-tweet-trends.js';
+import bootstrap from './observable-press.js';
+
+bootstrap(notebook);
