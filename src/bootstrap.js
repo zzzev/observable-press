a760c70590c87b9509b9d9cd806b07e8637fca74
Zev Youra <zevyoura@gmail.com>
2019-03-14 14:51:00 -0700

    whitespace tweak

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,10 +1,11 @@
 import bootstrap from './observable-press.js';
 
 const notebookAttribute = 'data-notebook';
-
 const notebookId = document.querySelector(`[${notebookAttribute}]`).getAttribute(notebookAttribute);
 
 const loadAll = document.querySelector('[data-load-all]') !== null;
+
 const overrideHeight = document.querySelector('[data-override-height]') !== null;
+
 import(`https://api.observablehq.com/${notebookId}.js`)
   .then(notebook => bootstrap(notebook.default, {loadAll, overrideHeight}));
