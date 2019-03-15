3914d13dc3dd9fc7621d0e907d49d0c9d09b69d3
Zev Youra <zevyoura@gmail.com>
2019-03-14 18:06:24 -0700

    some unnecessary but not harmful polish, discovered while debugging observable api issue

--- src/shimport.js
+++ src/shimport.js
@@ -3,6 +3,7 @@ function shimport(src) {
     new Function('import("' + src + '")')();
   } catch (e) {
     var s = document.createElement('script');
+    s.setAttribute('type', 'module');
     s.src = 'https://unpkg.com/shimport';
     s.dataset.main = src;
     document.head.appendChild(s);
